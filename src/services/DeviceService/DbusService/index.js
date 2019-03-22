789d756e4b47736528bcd9cc52246066634f1eb1
Ramon Henrique <rhpr@cesar.org.br>
2019-03-21 23:51:52 -0300

    Add initial dbus service

---
+++ src/services/DeviceService/DbusService/index.js
@@ -0,0 +1,15 @@
+class DbusServices {
+  constructor(config) { // eslint-disable-line no-unused-vars, no-useless-constructor
+  }
+
+  execute() { // eslint-disable-line class-methods-use-this
+  }
+
+  listDevices() { // eslint-disable-line class-methods-use-this
+  }
+
+  getDevice(id) { // eslint-disable-line no-unused-vars, class-methods-use-this
+  }
+}
+
+export default DbusServices;

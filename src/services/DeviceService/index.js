59f71cc3f75c193ec77a2c643a783eecc27317ad
Ramon Henrique <rhpr@cesar.org.br>
2019-03-21 23:26:49 -0300

    Add initial support to device service
    This patch add placeholder functions in an abstract layer to allow
    multprotocols to backend

---
+++ src/services/DeviceService/index.js
@@ -0,0 +1,22 @@
+class DeviceService {
+  constructor(protocol, config) { // eslint-disable-line no-unused-vars
+    switch (protocol) {
+      case 'DBUS':
+        // init service
+        break;
+      default:
+        break;
+    }
+  }
+
+  execute() { // eslint-disable-line class-methods-use-this
+  }
+
+  listDevices() { // eslint-disable-line class-methods-use-this
+  }
+
+  getDevice(id) { // eslint-disable-line no-unused-vars, class-methods-use-this
+  }
+}
+
+export default DeviceService;
